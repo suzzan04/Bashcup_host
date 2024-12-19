f65a00371dba7bb07a1ce3a2b2bd285bdee76581
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-12-19 18:32:49 +0545

    update: menu ui

--- src/components/Landing/Menu.tsx
+++ src/components/Landing/Menu.tsx
@@ -8,7 +8,7 @@ const Menu = () => {
   const menuData: Item[] = specials;
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <div className="w-full h-full max-w-[1800px] px-5 py-10 flex flex-col gap-8 bg-background">
+      <div className="w-full h-full max-w-[1800px] px-5 py-16 flex flex-col gap-10 bg-background">
         <h3 className="text-xl text-center font-semibold tracking-wider px-3 w-full py-6 ">
           COFFEE
         </h3>
