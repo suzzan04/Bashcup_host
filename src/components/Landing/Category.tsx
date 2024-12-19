1abb8758b63e7ba3a9abf5ded363e28e5909d645
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-12-19 18:31:53 +0545

    update: category ui

--- src/components/Landing/Category.tsx
+++ src/components/Landing/Category.tsx
@@ -13,26 +13,28 @@ const Category = () => {
   const categoriesData: Categories[] = category;
   return (
     <div className="w-full h-full flex justify-center items-center ">
-      <div className="w-full h-full max-w-[1800px] bg-background px-8 py-10 flex flex-col gap-14">
-        <h3 className="text-xl text-left font-semibold tracking-wider px-3 ">
+      <div className="w-full h-full max-w-[1800px] bg-background px-8 py-16 flex flex-col gap-14">
+        <h3 className="text-xl text-center font-semibold tracking-wider px-3 ">
           CATEGORIES
         </h3>
-        <div className="w-full px-3 flex gap-5 justify-between flex-wrap ">
+        <div className="w-full px-3 grid grid-cols-2 gap-5 lg:grid-cols-4">
           {categoriesData.map((data, index) => (
             <div
-              className=" rounded-full  flex flex-col gap-2 cursor-pointer items-center hover:scale-125 roverflow-hidden"
+              className="  flex flex-col gap-2 cursor-pointer items-center "
               key={index}
             >
-              <Image
-                src={data.image}
-                alt={data.name}
-                width={300}
-                height={400}
-                className="object-contain object-center size-32 rounded-full overflow-hidden"
-              />
-              <p className="text-sm tracking-wide text-brand-accent_dark">
-                {data.name.toLocaleUpperCase()}
-              </p>
+              <div className="w-full h-full relative  max-w-[300px] min-w-[150px]  overflow-hidden hover:scale-110 ">
+                <Image
+                  src={data.image}
+                  alt={data.name}
+                  width={600}
+                  height={600}
+                  className=" object-center w-full h-full object-cover  overflow-hidden rounded-2xl"
+                />
+                <p className="tracking-wide text-background font-bold  absolute bg-[#0303038a] w-full h-full max-w-[300px] rounded-2xl top-0 left-0 flex justify-center items-center">
+                  {data.name.toLocaleUpperCase()}
+                </p>
+              </div>
             </div>
           ))}
         </div>
