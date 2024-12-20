a00e70879d0fd1802272de9fed19cbb4ec502d22
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-12-20 21:07:20 +0545

    ui: decrease font size of top navbar text

--- src/components/Navbar/TopNavbar.tsx
+++ src/components/Navbar/TopNavbar.tsx
@@ -10,7 +10,7 @@ const TopNavbar = () => {
       <div className="w-full flex justify-center sm:justify-between items-center gap-8">
         <div className="hidden sm:flex items-center gap-3">
           <CircleIcon icon={<Phone />} size={9} />
-          <p className="text-sm  tracking-wider">+977-9813425299</p>
+          <p className="text-xs  tracking-wider">+977-9813425299</p>
         </div>
         <div className="flex items-center tracking-widest gap-1 flex-nowrap select-none">
           <Image
@@ -25,7 +25,10 @@ const TopNavbar = () => {
         </div>
         <div className="hidden sm:flex items-center gap-3">
           <CircleIcon icon={<Truck />} size={9} />
-          <p className="text-sm  tracking-wider">Delivery Available</p>
+          <p className="tracking-wider w-[150px] text-xs flex flex-col text-nowrap">
+            Delivery all over
+            <span>Kathmandu valley.</span>
+          </p>
         </div>
       </div>
     </div>
